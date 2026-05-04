c12a6cff278cec01ac766ae05500adcf6ddef8bf
Ismael Marín <ismael.marin@gmail.com>
2026-05-04 12:02:28 -0600

    Phase 3B: Standalone Evaluator Extraction (#47)
    * Phase 3B: Standalone Evaluator Extraction
    
    * Refactor

---
+++ evaluator/test/evaluator/gemspec_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Evaluator
+  class GemspecTest < Minitest::Test
+    def setup
+      Dir.chdir(File.expand_path('../../..', __dir__)) do
+        @spec = Gem::Specification.load('evaluator/agent_evaluator.gemspec')
+      end
+    end
+
+    def test_package_metadata_points_to_project_sources
+      assert_equal 'https://github.com/igmarin/rails-agent-skills', @spec.homepage
+      assert_equal 'https://github.com/igmarin/rails-agent-skills/tree/main/evaluator',
+                   @spec.metadata['source_code_uri']
+      assert_equal 'true', @spec.metadata['rubygems_mfa_required']
+    end
+
+    def test_package_includes_readme_and_license
+      assert_includes @spec.files, 'README.md'
+      assert_includes @spec.files, 'LICENSE'
+    end
+
+    def test_package_includes_evaluator_lib_files_when_loaded_from_repo_root
+      assert_includes @spec.files, 'lib/evaluator/version.rb'
+      assert_includes @spec.files, 'lib/runner.rb'
+    end
+  end
+end
